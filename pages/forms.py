7a281d7b206f3bd1d6f2ee2db925470dec938006
ian-garciso <aldriangarciso08@gmai.com>
2025-10-20 20:16:06 +0800

    Initial commit - my version

---
+++ pages/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'body']
+    
+    def clean_title(self):
+        t = self.cleaned_data['title']
+        if "test" in t.lower():
+            raise forms.ValidationError("Title cannot contain the word 'test'.")
+        if len(t) < 3:
+            raise forms.ValidationError('Title too short.')
+        return t
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['author', 'text']
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your name'}),
+            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Write a comment...'}),
+        }
